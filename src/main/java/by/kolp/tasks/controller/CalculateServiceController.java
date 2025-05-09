35cd9ffc6d0e2667fbf23d16ab141bc616bbb4de
kolpDeveloper <hasta21q@gmail.com>
2025-05-10 00:28:06 +0500

    Calculation controller, not finished yet

--- src/main/java/by/kolp/tasks/controller/CalculateServiceController.java
+++ src/main/java/by/kolp/tasks/controller/CalculateServiceController.java
@@ -1,10 +1,13 @@
 package by.kolp.tasks.controller;
 
+import by.kolp.tasks.entity.NumericDataEntry;
 import by.kolp.tasks.service.CalculateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/calculator")
@@ -14,5 +17,23 @@ public class CalculateServiceController {
     private CalculateService service;
 
 
+
+    public String showForm(Model model) {
+        model.addAttribute("numericDataEntry", new NumericDataEntry());
+        return "calculation"; //html
+
+    }
+
+
+    @PostMapping("/calculate")
+    public String calculate(@ModelAttribute("numericDataEntry") NumericDataEntry data, Model model) {
+
+        String key = data.getKey();
+        Integer value = data.getValue();
+
+        //TODO
+
+        return null;
+    }
 
 }
